dbc08eaa8b81ec125e6e96fda7f775ae3a9c6960
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2022-07-07 23:09:30 -0500

    fin de seccion

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,33 +1,47 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react"
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 import Image from "next/image";
+import NextLink from "next/link";
 
 export const Navbar = () => {
-
-    const { theme } = useTheme();
+  const { theme } = useTheme();
 
   return (
-    <div style={{
-        display: 'flex',
-        width: '100%',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'start',
-        padding: '0px 20px',
-        backgroundColor: theme?.colors.gray400.value
-    }}>
-        <Image 
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
-            alt="App Icon"
-            width={70}
-            height={70}
-        />
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "start",
+        padding: "0px 20px",
+        backgroundColor: theme?.colors.gray400.value,
+      }}
+    >
+      <Image
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png"
+        alt="App Icon"
+        width={70}
+        height={70}
+      />
 
-        <Text color="white" h2>P</Text>
-        <Text color="white" h3>okémon</Text>
+      <NextLink href="/" passHref>
+        <Link style={{ display: "flex" }}>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okémon
+          </Text>
+        </Link>
+      </NextLink>
 
-        <Spacer css={{ flex: 1}}/>
+      <Spacer css={{ flex: 1 }} />
 
-        <Text color="white">Favoritos</Text>
+      <NextLink href="/favoritos" passHref>
+        <Link>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
     </div>
-  )
-}
+  );
+};
---
+++ pages/favoritos/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '../../components/layouts'
+
+const FavoritosPage = () => {
+  return (
+    <Layout title='Pokemons - Favoritos'>
+        <h1>Ya valio Barriga</h1>
+    </Layout>
+  )
+}
+
+export default FavoritosPage
